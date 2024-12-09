547e3e58a6d677db6e7f86aa31a25393dbb759f5
chayko-anna <al.nchko@gmail.com>
2024-12-09 23:58:41 +0300

    finished(probably)

--- conftest.py
+++ conftest.py
@@ -1,9 +1,20 @@
 import os
+from asyncio import wait_for
 from zipfile import ZipFile
-
 import pytest
+import time
 
-@pytest.fixture(scope='class')
-
+@pytest.fixture()
 def zip_files():
-    files = []
+    CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+    res_dir = os.path.join(CURRENT_DIRECTORY, 'res')
+    zip_path = os.path.abspath("res/files.zip")
+
+    with ZipFile('res/files.zip', 'w') as res_zip:
+        for file in os.listdir(res_dir):
+            if ".zip" not in file:
+                add_file = os.path.join(res_dir, file)
+                res_zip.write(add_file, arcname=file)
+    yield
+
+    os.remove(zip_path)
--- test_files.py
+++ test_files.py
@@ -1,43 +1,46 @@
 from zipfile import ZipFile
-
 import openpyxl
 from pypdf import PdfReader
-from openpyxl import load_workbook
 import csv
 import os
 
 
-def test_files():
-    namelist = ["morr.pdf", "csv_tab.csv", "tab.xlsx"]
-    CURRENT_FILE = os.path.abspath(__file__)
-    CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE)
+def assert_path():
+    zip_path = os.path.abspath("res/files.zip")
+    return zip_path
 
-    zip_path = os.path.join(CURRENT_DIRECTORY, 'files.zip')
-    
-    if ".xlsx" in zip_path:
-        with ZipFile(zip_path) as zip_file:
-            with zip_file.open(zip_path, "r") as file:
-                excel_file = openpyxl.load_workbook(file)
-                excel_sheet = excel_file.active
-                text = []
-                for row in excel_sheet.iter_rows(values_only=True):
-                    text.append(row)
-                excel_out = ', '.join([', '.join(row) for row in text])
-                return excel_out
 
-    if ".csv" in zip_path:
-        with ZipFile(zip_path) as zip_file:
-            with zip_file.open(zip_path, "r") as file:
-                csv_content = file.read().decode('utf-8')
-                csv_reader = csv.reader(csv_content.splitlines())
-                text = []
-                for row in csv_reader:
-                    text.append(row)
-                csv_out = ', '.join([', '.join(row) for row in text])
-                return csv_out
+def test_file_pdf(zip_files):
+    zip_path = assert_path()
+    zip_file = ZipFile(zip_path)
+    pdf_zip = zip_file.open('morr.pdf')
+    pdf_file = PdfReader(pdf_zip)
+    assert 'The purpose and significance of this paper' in pdf_file.pages[0].extract_text()
+    pdf_zip.close()
+    zip_file.close()
 
-    if ".pdf" in zip_path:
-        with ZipFile(zip_path) as zip_file:
-            with zip_file.open(zip_path, "r") as file:
-                pdf_file = PdfReader(file)
-                return pdf_file.pages[0].extract_text()
+
+def test_file_csv(zip_files):
+    zip_path = assert_path()
+    zip_file = ZipFile(zip_path)
+    csv_file = zip_file.open('csv_tab.csv')
+    csv_content = csv_file.read().decode('utf-8')
+    csv_reader = csv.reader(csv_content.splitlines())
+    text = []
+    csv_out = []
+    for row in csv_reader:
+        text.append(row)
+        csv_out = ', '.join([', '.join(row) for row in text])
+    assert "Павлова" in csv_out
+    zip_file.close()
+
+
+def test_file_xlsx(zip_files):
+    zip_path = assert_path()
+    zip_file = ZipFile(zip_path)
+    xlsx_file = zip_file.open('tab.xlsx')
+    excel_file = openpyxl.load_workbook(xlsx_file)
+    excel_sheet = excel_file.active
+    assert "Дубина" in excel_sheet.cell(row=9,column=1).value
+    xlsx_file.close()
+    zip_file.close()
